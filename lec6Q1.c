2b2e82d5fc3f932c74b9a3e435a35892e780a05d
WW <ww@example.com>
2026-04-12 04:19:43 +0530

    Initial commit: add positive number check program

---
+++ lec6Q1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+    int num;
+    printf("Enter a Number:");
+    scanf("%d",&num);
+    
+    if(num>0)
+    {
+        printf("Number is Positive");
+    }
+
+
+    return 0;
+}
