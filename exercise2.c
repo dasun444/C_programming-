c52c80f3be7e2f349041dbf5cae6c82e4862f70f
dasun444 <dasunrajapaksha8396@gmail.com>
2026-04-12 17:37:14 +0530

    Update exercise2.c

--- exercise2.c
+++ exercise2.c
@@ -2,7 +2,7 @@
 int main(void) {
     float total;
     float discount=0.1;
-    printf("Enter the tota amount: ");
+    printf("Enter the total amount: ");
     scanf("%f",&total);
     if(total>1000){
             total = total - (total*discount);
