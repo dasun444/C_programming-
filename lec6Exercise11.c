bd13127a89aed106e06c5ace563aedfeebf40e98
WW <ww@example.com>
2026-04-13 14:26:13 +0530

    Else if  Ladder 3

---
+++ lec6Exercise11.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(void){
+    int foreignLoginStatus;
+    float transactionAmount;
+    printf("Enter your transaction amount: ");
+    scanf("%f",&transactionAmount);
+    printf("Enter your foreign login status(1=Yes, 0=No): ");
+    scanf("%d",&foreignLoginStatus);
+
+        if(transactionAmount>100000 && foreignLoginStatus==1){
+            printf("High Risk Transaction - Block Account");
+        }
+        else if(transactionAmount>100000 || foreignLoginStatus==1){
+            printf(" Security Verification Required");
+        }
+        else if(transactionAmount>=50000 && transactionAmount<=100000){
+            printf("Transaction Under Review");
+        }
+
+        else {
+            printf("Transaction Approved");
+        }
+
+}
