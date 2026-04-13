fa1eaf2eedcb6bf0fafda1ce69891a689e7c6d85
WW <ww@example.com>
2026-04-13 15:32:40 +0530

    Update files

--- example1.c
+++ example1.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 int main (void){
-    int x=10;
-if(x>5){
+    int x=4;
+if(x>5)
     printf(" x is greater than 5");
 
-}
+
+else  
+printf(" x is less than or equal to 5");
 }
  
