33961c71911ec681c33e8f49e1fa1c1cd7bf03f6
WW <ww@example.com>
2026-04-15 15:50:44 +0530

    Lecture 7 - Switch Statement (Char example)

---
+++ lec7Example3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(void){
+    char i = 'b';
+    switch(i){
+        case 'A':
+            printf(" I am in case 1\n");
+            break;
+        case 'B':
+            printf(" I am in case 2\n");
+            break;
+        case 'C':
+            printf(" I am in case 3\n");
+            break;
+        default:
+            printf(" I am in default\n"); 
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
