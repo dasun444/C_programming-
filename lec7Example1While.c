265486c685706e4b4ba6306b4c9d1f63423bd53c
WW <ww@example.com>
2026-04-16 23:41:59 +0530

    Lecture 7 - While(Infinite Loop( \n & no \n))

--- lec7Example1While.c
+++ lec7Example1While.c
@@ -2,7 +2,7 @@
 int main (void){
     int i=1;
     while (i<=10){
-        printf("%d ", i);
+        printf("%d ", i);//\n is used to print each number on a new line, but here we want to print them on the same line with a space in between
         // when i++ is not used, the value of i will always be 1 and the loop will run indefinitely
     }
 
