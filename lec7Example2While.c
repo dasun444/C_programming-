962622188287d258f5e8a6570e37efbe5641127c
WW <ww@example.com>
2026-04-16 23:49:07 +0530

    Lecture 7 - While 2nd example

---
+++ lec7Example2While.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (void){
+    int i=1;
+
+    while(i<=10){
+        printf("%d\n",i);// \n is used to print each number on a new line, but here we want to print them on the same line with a space in between
+        i++;
+    }
+}
