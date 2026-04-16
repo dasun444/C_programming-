75ac02c420149348ec0e4c44446192a8ffa7569a
WW <ww@example.com>
2026-04-17 01:51:32 +0530

    Lecture 7 - *****While (i++ vs ++i differance)

---
+++ lec7Example4While.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (void){
+    int i = 0;
+    while(i++<5){
+        printf("%d\n",i);
+    }
+    printf(" \n");
+    
+     int j = 0;
+    while(++j<5){
+        printf("%d\n",j);
+    }
+
+}
