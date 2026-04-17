0d07e707c3f1895c8f6f605639b6a5777a43f76c
WW <ww@example.com>
2026-04-17 13:35:31 +0530

    Lecture 7 - Task 5***** (Sum of positive numbers(Until negaive))

--- lec7Task5.c
+++ lec7Task5.c
@@ -12,3 +12,22 @@ int main (void){
     }
     printf("Sum of the entered numbers is: %d", sum);
 }
+/* Method 2 -using while and  if else statement
+#include <stdio.h>
+int main (void){
+int num=0;
+int sum=0;
+    while(num>=0){
+        printf("Enter a Number: ");
+        scanf("%d",&num);
+            if(num>=0){
+                sum = sum + num; //(sum+=num)
+        }
+            else{
+                printf("The number you entered is negative.\n sum of the entered numbers is: %d", sum);}
+    }
+}
+
+}
+
+*/
