f2144cfcfa42a950ed6ab3fc66b885ff192b3ed3
WW <ww@example.com>
2026-04-17 19:43:37 +0530

    Lecture 7 - StarPattern 5**

--- lec7StarPattern5.c
+++ lec7StarPattern5.c
@@ -16,3 +16,22 @@ int main (void){
 5 4 3 2 1 
 5 4 3 2 1 
 */
+
+/*#include <stdio.h>
+int main (void){
+    int i,j;
+    for (i=5;i>=1;i--){
+        for(j=5;j>=1;j--){
+            printf("%d ",i);
+        }
+    printf("\n");    
+}
+}
+ output
+5 5 5 5 5
+4 4 4 4 4
+3 3 3 3 3
+2 2 2 2 2
+1 1 1 1 1
+
+*/
