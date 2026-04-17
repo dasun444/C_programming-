ee347f545e5023bfc825a2ccfce43154dc9127fa
WW <ww@example.com>
2026-04-17 18:43:14 +0530

    Lecture 7 - StarPattern 1(Nested For loop)

---
+++ lec7StarPattern1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main (void){
+    int i,j;
+    for(i=1;i<=5;i++){
+
+        for(j=1;j<=5;j++){//i or j = 0 is not used here,then 6 numbers appear in each row 
+            printf("%d ",j);
+
+        }
+        printf("\n");
+    }
+
+/* Pattern Structure
+ int i,j;
+ for(){
+    for(){
+        
+    }
+    printf("\n");
+ 
+ }
+*/
+
+
+
+}
