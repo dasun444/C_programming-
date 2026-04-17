baf6e90393d1db542d8c38ce29c6a7e5f0427340
WW <ww@example.com>
2026-04-18 01:42:13 +0530

    Lecture 7- StarPattern12 ***

---
+++ lec7StarPattern13.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main (void){
+    int i,j,space,rows=5;
+    for (i=1;i<=5;i++){
+        for(space=1;space<=(rows-i);space++){
+            printf(" ");
+        }
+        for(j=1;j<=i;j++){
+            printf("* ");//  *(star + space )
+        }
+        printf("\n");
+    }
+}
+/*
+  output
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * *
+
+*/
