b96635c045a1655e337a4b6b3417c4aeaaa58b0d
dasun444 <dasunrajapaksha8396@gmail.com>
2026-04-21 18:45:54 +0530

    Lecture 8 - Task 2 (Summation of three numbers using Function)

--- lec8Task2.c
+++ lec8Task2.c
@@ -14,3 +14,26 @@ int sum(int x, int y , int z){//curly brackets
     return x+y+z;//**********/
 
 }
+/*
+#include <stdio.h>
+
+// function declaration
+int sumThree(int a, int b, int c);
+
+int main() {
+    int n1, n2, n3, result;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &n1, &n2, &n3);
+
+    result = sumThree(n1, n2, n3);
+
+    printf("Sum = %d", result);
+    return 0;
+}
+
+// function definition
+int sumThree(int a, int b, int c) {
+    return a + b + c;
+}
+*/
